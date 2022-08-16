0d331ed5f9cfb24c52a3e41bf9406b33ffd10aa4
vasilymarchenko <vasily.a.marchenko@gmail.com>
2022-08-17 00:29:26 +0300

    Update FluentValidatorTests.cs
    CascadeMode works only in case ClassLevelCascadeMode = CascadeMode.Stop;
    RuleLevel doesn't work

--- tests/UnitTests/Validation/FluentValidatorTests.cs
+++ tests/UnitTests/Validation/FluentValidatorTests.cs
@@ -13,16 +13,19 @@ namespace UnitTests.Validation
     {
         public MyModelValidator()
         {
+            ClassLevelCascadeMode = CascadeMode.Stop;
+
             // CascadeMode doesn't works.
             // Either in cascade notation either in sequence 
-            RuleFor(x => x.ModelName).Cascade(CascadeMode.Stop).NotNull().NotEmpty();
+            RuleFor(x => x.ModelName).Cascade(CascadeMode.Stop).NotNull().NotEmpty()
                 
             //It was expected the next steps will not check if CascadeMode is stop, but not...
 
-                //.When(x => x.ModelName.Length > 5).WithMessage("Name must be greater than 5")
-                //.When(x => x.ModelName.Substring(1).Length > 0).WithMessage("Substring must works ))");
-            RuleFor(x => x.ModelName).Must(s => s.Length > 5).WithMessage("Name must be greater than 5")
+                .When(x => x.ModelName.Length > 5).WithMessage("Name must be greater than 5")
                 .When(x => x.ModelName.Substring(1).Length > 0).WithMessage("Substring must works ))");
+
+        //    RuleFor(x => x.ModelName).Must(s => s.Length > 5).WithMessage("Name must be greater than 5")
+        //        .When(x => x.ModelName.Substring(1).Length > 0).WithMessage("Substring must works ))");
         }
     }
     
