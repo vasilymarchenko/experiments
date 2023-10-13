fb32b1ea68601bda4f9b101ea71500922567dd67
vasilymarchenko <vasily.a.marchenko@gmail.com>
2023-10-13 16:31:20 +0300

    Example serializable model with Dictionary

---
+++ src/ConsoleApp1/MyModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace ConsoleApp1
+{
+    internal class MyModel
+    {
+        private Dictionary<string, string> _settings = new Dictionary<string, string>();
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        //[JsonExtensionData]
+        public Dictionary<string, string> Settings
+        {
+            get
+            {
+                return _settings;
+            }
+            set
+            {
+                // Deserialize the dictionary from the provided JsonElement dictionary
+                _settings = new Dictionary<string, string>();
+                foreach (var kvp in value)
+                {
+                    _settings[kvp.Key] = kvp.Value;
+                }
+            }
+        }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static MyModel FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<MyModel>(json);
+        }
+    }
+}
---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+
+MyModel model1 = new MyModel()
+{
+    Id = "some-id",
+    Name = "Name",
+    Settings = new Dictionary<string, string>()
+    {
+        { "setting1", "value1" },
+        { "setting2", "value2" }
+    }
+};
+
+var jsonString = model1.ToJson();
+Console.WriteLine(jsonString);
+
+MyModel deserializedModel = MyModel.FromJson(jsonString);
+
+Console.WriteLine($"Id: {deserializedModel.Id}");
+Console.WriteLine($"Name: {deserializedModel.Name}");
+
+foreach (var setting in deserializedModel.Settings)
+{
+    Console.WriteLine($"{setting.Key}: {setting.Value}");
+}
